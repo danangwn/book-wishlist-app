d0a80ca9568cbd604397e5174d9a4568eabf9b72
Danang Wahyu Nugraha <danangwahyu.nugraha@gmail.com>
2023-01-08 17:02:29 +0700

    db online

--- prisma/migrations/20230108092127_initail/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "wishlist" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "book_id" TEXT NOT NULL,
-    "added_date" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "wishlist_id_key" ON "wishlist"("id");
