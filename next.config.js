60fe074553566f02bf7c22b1a710e65b43f06013
Danang Wahyu Nugraha <danangwahyu.nugraha@gmail.com>
2023-01-08 17:45:28 +0700

    new branch

--- next.config.js
+++ next.config.js
@@ -5,8 +5,6 @@ const nextConfig = {
     domains: ['books.google.com', 'www.pngfind.com'],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 }
